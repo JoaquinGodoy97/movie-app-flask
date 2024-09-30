4ecd447d1ea4678754e2d0a51e95c61a34c2d390
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2024-09-30 06:32:00 -0300

    working with pagination. Functionality takes page from url, seems to be working fine. Then follows make the wishlist work.

--- client/src/components/ResultsPage.js
+++ client/src/components/ResultsPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom'; // Import useLocation to access the query
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom'; // Import useLocation to access the query
 import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import PaginationPanel from './PaginationPanel';
@@ -8,7 +7,6 @@ import '../Main.css';
 
 const ResultsPage = () => {
     const [movies, setMovies] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const navigate = useNavigate();
 
@@ -16,6 +14,7 @@ const ResultsPage = () => {
     const location = useLocation();
     const queryParams = new URLSearchParams(location.search);
     const searchQuery = queryParams.get('query') || '';
+    const currentPageUrl = parseInt(queryParams.get('page')) || 1;
 
     const fetchMovies = async (query, page = 1) => {
         try {
@@ -39,10 +38,11 @@ const ResultsPage = () => {
 
     // This effect runs whenever the searchQuery or currentPage changes
     useEffect(() => {
-        if (searchQuery) {
-            fetchMovies(searchQuery, currentPage); // Trigger the fetch with the search query
+        if (searchQuery && currentPageUrl) {
+            fetchMovies(searchQuery, currentPageUrl); 
         }
-    }, [searchQuery, currentPage]);
+
+    }, [searchQuery, currentPageUrl]);
 
     const handleSearch = (query) => {
         if (!query) {
@@ -50,14 +50,12 @@ const ResultsPage = () => {
             return;
         }
 
-        setCurrentPage(1)
         // Navigate to the results page with the search query
         navigate(`/results/search?query=${query}&page=1`);
     };
 
     const handlePageChange = (newPage) => {
         if (newPage > 0 && newPage <= totalPages) {
-            setCurrentPage(newPage);
             // Add logic here to fetch new data based on the page (if needed)
             console.log(`Fetching data for page: ${newPage}`);
             navigate(`/results/search?query=${searchQuery}&page=${newPage}`);
@@ -73,10 +71,10 @@ const ResultsPage = () => {
         <div className="results-page">
             <SearchBar
                 onSearch={handleSearch}
-                currentPage={currentPage}
+                currentPage={currentPageUrl}
                 totalPages={totalPages}
             />
-            <PaginationPanel currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} />
+            <PaginationPanel currentPage={currentPageUrl} totalPages={totalPages} onPageChange={handlePageChange} />
 
             <MovieList movies={movies} />
         </div>
