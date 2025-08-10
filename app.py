12b10d94b400c46c74259d29845b66af71e242d7
Joaquin Godoy <godoy.joaquin1997@gmail.com>
2025-08-10 06:47:18 -0300

    run front with message error for context

--- app.py
+++ app.py
@@ -7,12 +7,16 @@ app = create_app()
 
 with app.app_context():
 #     from server.utils.db import db
-#     db.create_all()
-    create_database()
-    initialize_connection_pool()
-    # with app.app_context():
-    create_users_table()
-    create_wishlist_user_table()
+#   
+#   db.create_all()
+    try:
+        create_database()
+        initialize_connection_pool()
+        create_users_table()
+        create_wishlist_user_table()
+    except:
+        print("Database is not working correctly.")
+        # with app.app_context():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
